b7df2d0e4406595560f19adbd33839d65de58b67
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2022-08-30 15:32:03 +0800

    upload

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Assuming all Development tools are installed and proper 
+# GCC settings are set, run the script.
+# [Taken from DragonflyDB Github Repo]
+
+check="N"
+
+echo "Please ensure that you have all the proper dependencies installed."
+echo "If you do not have all the dependencies installed, please install them."
+echo "Dependencies Installed? (y/N)"
+read check
+
+if [ $check = "n" ] || [ $check = "N" ]; then
+    echo "Please install all the dependencies and run the script again."
+    exit
+else
+    git clone --recursive https://github.com/dragonflydb/dragonfly && cd dragonfly
+    ./helio/blaze.sh -release
+    cd build-opt
+    ninja dragonfly
+
+    cp dragonfly /usr/bin/
+    cd ../..
+    cp dragonfly.service /usr/lib/systemd/system/
+
+    echo "Dragonfly installed. run 'systemctl start dragonfly' to start the service."
+fi
+
+
